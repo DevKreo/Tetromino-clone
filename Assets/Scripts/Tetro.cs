dce5f4520fe0ba98e82bd043a51190181074d618
Tim Shishunov <devkreo@gmail.com>
2019-12-13 20:30:14 +0300

    Add tochscreen support
    Find a bug phantom tetromino. I must fix them.

--- Assets/Scripts/Tetro.cs
+++ Assets/Scripts/Tetro.cs
@@ -11,6 +11,13 @@ public class Tetro : MonoBehaviour
     public int individualScore = 100;
     private float individualScoreTime;
 
+    //Переменные для тачскрина
+    private int touchSensitivityHorizontal = 8;
+    private int touchSensitivityVertical = 4;
+    Vector2 previosUnitPosition = Vector2.zero;
+    Vector2 direction = Vector2.zero;
+    bool isMoved = false;
+
     private void Start()
     {
         fallSpeed = GameObject.Find("GameScript").GetComponent<Game>().fallSpeed;
@@ -37,103 +44,77 @@ public class Tetro : MonoBehaviour
 
     void CheckUserInput()
     {
+
+        #if UNITY_ANDROID
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+                previosUnitPosition = new Vector2(touch.position.x, touch.position.y);
+
+            }else if(touch.phase == TouchPhase.Moved)
+            {
+                Vector2 tochDeltaPosition = touch.deltaPosition;
+                direction = tochDeltaPosition.normalized;
+
+                if(Mathf.Abs(touch.position.x - previosUnitPosition.x) >= touchSensitivityHorizontal && direction.x < 0 && touch.deltaPosition.y > -10 && touch.deltaPosition.y <10)
+                {
+                    //left
+                    MoveLeft();
+                    previosUnitPosition = touch.position;
+                    isMoved = true;
+
+                } else if (Mathf.Abs(touch.position.x - previosUnitPosition.x) >= touchSensitivityHorizontal && direction.x > 0 && touch.deltaPosition.y > -10 && touch.deltaPosition.y < 10)
+                {
+                    //right
+                    MoveRight();
+                    previosUnitPosition = touch.position;
+                    isMoved = true;
+                } else if(Mathf.Abs(touch.position.y - previosUnitPosition.y) >= touchSensitivityVertical && direction.y < 0 && touch.deltaPosition.x > -10 && touch.deltaPosition.x < 10)
+                {
+                    //down
+                    MoveDown();
+                    previosUnitPosition = touch.position;
+                    isMoved = true;
+                }
+            }
+            else if(touch.phase == TouchPhase.Ended)
+            {
+                if(!isMoved && touch.position.x > Screen.width / 4)
+                {
+                    Rotate();
+                }
+                isMoved = false;
+            }
+        }
+
+        if(Time.time - fall >= fallSpeed){
+            MoveDown();
+        }
+
+        #else
+
         if (Input.GetKeyDown(KeyCode.RightArrow))
-        {   
-            
-
-            transform.position += new Vector3(1, 0, 0);
-
-            if (CheckIsValidPosition())
-            {
-                FindObjectOfType<Game>().UpdateGrid(this);
-            }
-            else
-            {
-                transform.position += new Vector3(-1, 0, 0);
-            }
+        {
+            MoveRight();
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.position += new Vector3(-1, 0, 0);
-            if (CheckIsValidPosition())
-            {
-                FindObjectOfType<Game>().UpdateGrid(this);
-            }
-            else
-            {
-                transform.position += new Vector3(1, 0, 0);
-            }
+            MoveLeft();
         }
         else if (Input.GetKey(KeyCode.DownArrow) || Time.time - fall >= fallSpeed)
         {
-            transform.position += new Vector3(0, -1, 0);
-            if (CheckIsValidPosition())
-            {
-                FindObjectOfType<Game>().UpdateGrid(this);
-            }
-            else
-            {
-                transform.position += new Vector3(0, 1, 0);
-
-                FindObjectOfType<Game>().DeleteRow();
-
-                if (FindObjectOfType<Game>().CheckPositionOnGameOver(this))
-                {
-                    FindObjectOfType<Game>().GameOver();
-                }
-
-                enabled = false;
-
-                Game.currentScore += individualScore;
-
-                FindObjectOfType<Game>().SpawnNextTetro();
-            }
-
-            fall = Time.time;
+            MoveDown();
         }
         else if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (isAllowRotation)
-            {
-                if (IsLimitRotation)
-                {
-                    if (transform.rotation.eulerAngles.z >= 90)
-                    {
-                        transform.Rotate(0, 0, -90);
-                    }
-                    else
-                    {
-                        transform.Rotate(0, 0, 90);
-                    }
-                }
-                else
-                {
-                    transform.Rotate(0, 0, 90);
-                }
-                if (CheckIsValidPosition())
-                {
-
-                }
-
-                else
-                {
-                    if (IsLimitRotation)
-                    {
-                        if (transform.rotation.eulerAngles.z >= 90)
-                        {
-                            transform.Rotate(0, 0, -90);
-                        }
-                        else
-                            transform.Rotate(0, 0, 90);
-                    }
-                    else
-                    {
-                        transform.Rotate(0, 0, -90);
-                    }
-                }
-            }
+            Rotate();
         }
-           
+
+        #endif
     }
 
     bool CheckIsValidPosition()
@@ -150,6 +131,104 @@ public class Tetro : MonoBehaviour
             }
         }
         return true;
+    }
+
+    void MoveLeft()
+    {
+        transform.position += new Vector3(-1, 0, 0);
+        if (CheckIsValidPosition())
+        {
+            FindObjectOfType<Game>().UpdateGrid(this);
+        }
+        else
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+    }
+
+    void MoveRight()
+    {
+        transform.position += new Vector3(1, 0, 0);
+
+        if (CheckIsValidPosition())
+        {
+            FindObjectOfType<Game>().UpdateGrid(this);
+        }
+        else
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+    }
+
+    void MoveDown()
+    {
+        transform.position += new Vector3(0, -1, 0);
+        if (CheckIsValidPosition())
+        {
+            FindObjectOfType<Game>().UpdateGrid(this);
+        }
+        else
+        {
+            transform.position += new Vector3(0, 1, 0);
+
+            FindObjectOfType<Game>().DeleteRow();
+
+            if (FindObjectOfType<Game>().CheckPositionOnGameOver(this))
+            {
+                FindObjectOfType<Game>().GameOver();
+            }
+
+            enabled = false;
+
+            Game.currentScore += individualScore;
+
+            FindObjectOfType<Game>().SpawnNextTetro();
+        }
+
+        fall = Time.time;
+    }
+
+    void Rotate()
+    {
+        if (isAllowRotation)
+        {
+            if (IsLimitRotation)
+            {
+                if (transform.rotation.eulerAngles.z >= 90)
+                {
+                    transform.Rotate(0, 0, -90);
+                }
+                else
+                {
+                    transform.Rotate(0, 0, 90);
+                }
+            }
+            else
+            {
+                transform.Rotate(0, 0, 90);
+            }
+            if (CheckIsValidPosition())
+            {
+
+            }
+
+            else
+            {
+                if (IsLimitRotation)
+                {
+                    if (transform.rotation.eulerAngles.z >= 90)
+                    {
+                        transform.Rotate(0, 0, -90);
+                    }
+                    else
+                        transform.Rotate(0, 0, 90);
+                }
+                else
+                {
+                    transform.Rotate(0, 0, -90);
+                }
+            }
+        }
     }
 }
 
